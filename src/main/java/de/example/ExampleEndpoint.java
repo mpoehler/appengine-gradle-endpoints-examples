1748c67f5b16a69b46131f9ca903ce046deabdd0
Marco Poehler <marco@kontaktlinsen-preisvergleich.de>
2016-10-21 15:24:10 +0200

    first working version

---
+++ src/main/java/de/example/ExampleEndpoint.java
@@ -0,0 +1,34 @@
+package de.example;
+
+/**
+ * Created by mpoehler@gmail.com on 21.10.16.
+ */
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.response.CollectionResponse;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Api(name = "myApi", version = "v1",
+        scopes = { "https://www.googleapis.com/auth/userinfo.email" },
+        clientIds = {
+                // this is to give the api explorer access
+                com.google.api.server.spi.Constant.API_EXPLORER_CLIENT_ID,
+
+                // this client id is created here: https://console.cloud.google.com/apis/credentials
+                // Use OAuth ClientID for Webapplication, maybe you need to configure the consent screen first (just add a product name)
+                // You'll need to add "Authorized JavaScript origins" in the form: https://appid.appspot.com
+                "1065372429076-g8por2o8vlug3dtgf1i7n96i5nq51d4b.apps.googleusercontent.com"
+        }
+)
+public class ExampleEndpoint {
+    @ApiMethod(name = "example.getName", path = "example.getName")
+    public Collection<String> getName() {
+        Set<String> set = new HashSet<>();
+        set.add("This is from Example Endpoint");
+        return set;
+    }
+}
